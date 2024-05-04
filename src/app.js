fd48686415331e43b9b62e8fc10b11cffe0751d0
sindhura14 <sindhu.kilaru14@gmail.com>
2024-05-04 05:29:53 +0000

    changes for generating cards

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["\u2660", "\u2665", "\u2666", "\u2663"];
+  const ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  let button = document.querySelector(".btn");
+  button.addEventListener("click", event => {
+    let suit = suits[Math.floor(Math.random() * suits.length)];
+    let topSuitDiv = document.querySelector("#topSuit");
+    let bottomSuitDiv = document.querySelector("#bottomSuit");
+    topSuitDiv.innerHTML = suit;
+    bottomSuitDiv.innerHTML = suit;
+
+    let rank = ranks[Math.floor(Math.random() * ranks.length)];
+    let rankDiv = document.querySelector("#rank");
+
+    if (rank == 10) {
+      rankDiv.style.left = "2.5rem";
+    } else {
+      rankDiv.style.left = "5rem";
+    }
+    rankDiv.innerHTML = rank;
+    if (suit == "\u2665" || suit == "\u2666") {
+      topSuitDiv.style.color = "red";
+      bottomSuitDiv.style.color = "red";
+      rankDiv.style.color = "red";
+    } else {
+      topSuitDiv.style.color = "";
+      bottomSuitDiv.style.color = "";
+      rankDiv.style.color = "";
+    }
+  });
 };
